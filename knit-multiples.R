a8264719411021edca0108dd795b010001d203d6
Kenneth Wong <khwongk12@gmail.com>
2021-12-28 07:18:48 +0800

    chore: Rename `ACCIDENT` to `COLLISION`

--- knit-multiples.R
+++ knit-multiples.R
@@ -2,15 +2,15 @@ library(knitr)
 library(markdown)
 library(rmarkdown)
 
-accident_serial_no = seq(1:95821)
+collision_serial_no = seq(1:95821)
 
-accident_serial_no_test = sample(accident_serial_no, 20)
+collision_serial_no_test = sample(collision_serial_no, 20)
 
 # https://community.rstudio.com/t/loop-for-output-files/79716/4
-for (i in accident_serial_no_test) {
+for (i in collision_serial_no_test) {
   rmarkdown::render(
     "detail-sample-page.Rmd",
     output_file = paste0("accident_", i, ".html"),
-    param = list(SELECTED_ACCIDENT_NO = i)
+    param = list(COLLISION_SERIAL_NO = i)
     )
 }
